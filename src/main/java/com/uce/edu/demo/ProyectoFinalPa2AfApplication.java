766a610e6e9cae9a140245691eafc95d04d426e7
Kevin Toapanta <kevin.toapanta997@gmail.com>
2022-09-12 17:35:59 -0500

    Proyecto Final

---
+++ src/main/java/com/uce/edu/demo/ProyectoFinalPa2AfApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalPa2AfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalPa2AfApplication.class, args);
+	}
+
+}
